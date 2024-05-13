6a071a21dd5a558cf239ee41b2fbd05e179e0fe2
Katy Kane <katytheartist@gmail.com>
2024-05-13 12:47:10 -0500

    wip

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,28 @@
-// STRETCH
+const cars = [
+    {
+        vin: '01234567891011123',
+        make: 'subaru',
+        model: 'crosstrek',
+        mileage: '50000',
+        title: 'clean',
+        transmission: 'auto'
+    },
+    {
+        vin: '01234567891055523',
+        make: 'kia',
+        model: 'sorento',
+        mileage: '5000',
+        transmission: 'manual'
+    },
+    {
+        vin: '01234567891011444',
+        make: 'honda',
+        model: 'civic',
+        mileage: '500000',
+    },
+]
+
+exports.seed = async function(knex){
+    await knex('cars').truncate()
+    await knex('cars').insert(cars)
+}
