833c29eec256a11a9b2342beaca90d3521f3691c
Katy Kane <katytheartist@gmail.com>
2024-05-13 13:20:35 -0500

    wip

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,6 +1,6 @@
 exports.up = function (knex) {
   return knex.schema.createTable('cars', tbl =>{
-    tbl.icrements()
+    tbl.increments()
 
     tbl.string('vin', 17).notNullable().unique()
 
@@ -8,7 +8,7 @@ exports.up = function (knex) {
 
     tbl.string('model', 128).notNullable()
 
-    tbl.numeric('mileage').unsigned().notNullable()
+    tbl.integer('mileage').unsigned().notNullable()
 
     tbl.string('title', 128)
 
