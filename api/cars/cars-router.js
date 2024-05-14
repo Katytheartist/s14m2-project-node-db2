f29bf07890a4e7b2dde02bd21b7a6ccdf971f061
Katy Kane <katytheartist@gmail.com>
2024-05-14 09:45:50 -0500

    wip

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const Car = require('./cars-model')
 
 router.get('/', async (req, res, next)=>{
-     res.json('getting all cars')
+    try{
+        const cars = await Car.getAll()
+        res.json(cars)
+    } catch(err){
+        next(err)
+    }
 })
 router.get('/:id', async (req, res, next)=>{
      res.json(`getting car with id ${req.params.id}`)
