89c640340e833b7e6c6c65dc8da4622a75cf0ff9
Katy Kane <katytheartist@gmail.com>
2024-05-13 21:58:14 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -5,7 +5,13 @@ const server = express()
 server.use(express.json())
 
 server.use('*', (req, res, next)=>{
-    next({status: 404, message: 'so sorry, not found darlin'})
+    next({status: 404, message: 'so sorry, not found darlin!'})
+})
+
+server.use((err, req, res, next)=>{ //eslint-disable-line
+    res.status(err.status || 500).json({
+        message: err.message
+    })
 })
 
 module.exports = server
